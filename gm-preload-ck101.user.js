614a9584158f6aaa28f30d2ac1240982a6c66d19
Jiasen <jiasen.low@oaxis.com>
2015-12-19 12:44:54 +0800

    initial commit

---
+++ gm-preload-ck101.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        ck101 images
+// @namespace   http://ck101.com/viewthread
+// @include     http://ck101.com/*
+// @include     http://www.ck101.com/*
+// @version     2
+// @grant       none
+// ==/UserScript==
+function onLoad() {
+    jQuery('img[file]') .each(function () {
+        $this = jQuery(this);
+        $this.attr('file', $this.attr('file') .concat('?!23'));
+        $this.attr('src', $this.attr('file'));
+    });
+    jQuery('a[href*="mod=viewthread"]') .each(function () {
+        $this = jQuery(this);
+        $this.attr('href', ($this.attr('href') .replace(/page%3D\d+/, '')));
+    });
+}
+window.addEventListener('load', onLoad, false);
